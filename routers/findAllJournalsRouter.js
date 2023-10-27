73567f544ceaae4b5fdba096f11a29756ab6363c
LuckyIAM <zavalisca.elena@gmail.com>
2023-10-27 10:46:05 +0300

    clean code from trash

--- routers/findAllJournalsRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = new express()
-const findAllJournalsController = require('../controllers/findAllJournalsController')
-
-
-router.post('/', findAllJournalsController.getJournals)
-
-module.exports = router
--- routers/index.js
+++ routers/index.js
@@ -3,13 +3,12 @@ const router = new express()
 const user = require('./userRouter')
 const importFile = require('./importFileRouter')
 const contentData = require('./contentDataRouter')
-const findJournals = require('./findAllJournalsRouter')
+
 
 
 router.use('/v1/importfile', importFile)
 router.use('/v1/user', user)
 router.use('/v1/contentdata', contentData)
-router.use('/v1/find/journal', findJournals)
 
 
 module.exports = router
